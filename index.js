b473a1745676de382f9cc0981665c9a3181944f2
jvitorn <joaoeuno2011@gmail.com>
2020-05-27 12:34:41 -0300

    comentario sobre a conversao de imagem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path    = require('path');
 const port     = 4200;
 const cors    = require('cors');
-const formidable = require('formidable');
+
 const fs = require('fs');
 
 //base64convert
@@ -31,24 +31,26 @@ const base64ToImage = require('base64-to-image');
 app.post('/upload', function(req, res) {
    const { title } = req.body;
    const { img } = req.body;
+   //verifica se no campo de imagem foi enviado algo 
    if(img){
       //verifica se o que esta sendo enviado é uma imagem
       const result  = img.indexOf('data:image/');
+      //caso 1 = imagem , caso -1 nao é uma imagem , portando nao pode ser transformada em img
       if(result == 0){
       var base64Str = img;
       //caminho das imagens
       var path ='C:/Users/Admin/Desktop/Projetos/client-upload/imgs/';
+      //filename é o nome do arquivo da img
       var optionalObj = {'fileName': title, 'type':'png'};
-   
+      //função de conversao 
       base64ToImage(base64Str,path,optionalObj); 
 
       
    // Note base64ToImage function returns imageInfo which is an object with imageType and fileName.
       var imageInfo = base64ToImage(base64Str,path,optionalObj); 
+      //caminho inteiro da imagem
       const caminhodaimg = path + imageInfo.fileName;
-      console.log(imageInfo);
-      console.log(caminhodaimg)
-      console.log(result)
+     
       }else{
          console.log('nao é uma imagem');
       }
