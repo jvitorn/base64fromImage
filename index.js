539c297d9984fb492cbbdb04daabb5f1860d2f87
jvitorn <joaoeuno2011@gmail.com>
2020-05-27 11:05:13 -0300

    adicionando base64 to image e verificando se é uma imagem

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port     = 4200;
 const cors    = require('cors');
 const formidable = require('formidable');
 const fs = require('fs');
-const fileUpload = require('express-fileupload');
+
+//base64convert
+const base64ToImage = require('base64-to-image');
 
  //criando app
  const app = express();
@@ -24,16 +26,36 @@ const fileUpload = require('express-fileupload');
     //cors
     app.use(cors())
 
- // default options
-app.use(fileUpload());
+
 
 app.post('/upload', function(req, res) {
    const { title } = req.body;
    const { img } = req.body;
    if(img){
-      console.log('imagem existe');
+      //verifica se o que esta sendo enviado é uma imagem
+      const result  = img.indexOf('data:image/');
+      if(result == 0){
+      var base64Str = img;
+      //caminho das imagens
+      var path ='C:/Users/Admin/Desktop/Projetos/client-upload/imgs/';
+      var optionalObj = {'fileName': title, 'type':'png'};
+   
+      base64ToImage(base64Str,path,optionalObj); 
+
+      
+   // Note base64ToImage function returns imageInfo which is an object with imageType and fileName.
+      var imageInfo = base64ToImage(base64Str,path,optionalObj); 
+      const caminhodaimg = path + imageInfo.fileName;
+      console.log(imageInfo);
+      console.log(caminhodaimg)
+      console.log(result)
+      }else{
+         console.log('nao é uma imagem');
+      }
+      
    }
-  console.log(title);
+   //passar a srt em base64
+   
 });
  app.listen(port,()=>{
     console.log('Servidor Iniciado e Rodando no link http://localhost:'+port);
